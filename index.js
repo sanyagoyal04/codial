c36d40e2678abbfca0ef9a3a4c8da27079ad59ce
sanyagoyal04 <sanya.goyal_ug23@nsut.ac.in>
2024-12-15 18:33:42 +0530

    set up view engine and views directory

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express= require('express');
 const app= express();
 const port =3000;
 app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log("error running server : ",err);
         return;
-    }
+    }a
     console.log('server running succesfully on port : ',port);
 })
